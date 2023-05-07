f2aa525ef23979044f864207ac9ccaa2bc7f1ff1
jonnebonde <jonnebonde@gmail.com>
2023-05-07 21:01:11 +0200

    started on add.html and add.js

---
+++ js/add.js
@@ -0,0 +1,22 @@
+import navBarMenu from "./components/ui/renderNavMenu.js";
+import userLogout from "./components/logout.js";
+import { getUserName, getToken } from "./components/utils/storage/userStorage.js";
+
+
+navBarMenu();
+userLogout();
+
+const token = getToken();
+const user = getUserName();
+
+console.log(user, token)
+
+if (user && token) {
+
+
+  
+} else {
+  location.href = "/index.html";
+}
+
+
